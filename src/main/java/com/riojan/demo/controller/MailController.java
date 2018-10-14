1d798215ec2b01c8ab919fa3a5d958f8267aa8c9
Maxi <maxi_ruiz140@hotmail.com>
2018-10-14 04:54:36 -0300

    log4j and CustomErrorController

--- src/main/java/com/riojan/demo/controller/MailController.java
+++ src/main/java/com/riojan/demo/controller/MailController.java
@@ -3,6 +3,7 @@ package com.riojan.demo.controller;
 import com.riojan.demo.model.MailModel;
 
 import com.sendgrid.*;
+import org.apache.log4j.Logger;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,8 @@ import java.io.IOException;
 @RequestMapping
 public class MailController {
 
+    private static Logger logger = Logger.getLogger(MailController.class);
+
     @RequestMapping(path="/email", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String sendEmail(@RequestBody MailModel emailJson) throws Exception{
         SendGrid sendgrid = new SendGrid("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");
@@ -24,8 +27,10 @@ public class MailController {
         Content content = new Content("text/plain", emailJson.getBody());
         String subject = emailJson.getSubject();
         Personalization personalization = new Personalization();
+        logger.info("To:");
         for(String aTo: emailJson.getTo()){
             personalization.addTo(new Email(aTo));
+            logger.info(aTo);
         }
 
         for(String aCc: emailJson.getCc()){
@@ -40,6 +45,11 @@ public class MailController {
         mail.addContent(content);
         mail.setFrom(from);
 
+        logger.info("Subject: " + subject);
+        logger.info("From: " + from);
+        logger.info("Content: ");
+        logger.info(content.getValue());
+
         mail.addPersonalization(personalization);
 
         Request request = new Request();
@@ -47,10 +57,12 @@ public class MailController {
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
+
             Response response = sendgrid.api(request);
             System.out.println(response.getStatusCode());
             System.out.println(response.getBody());
             System.out.println(response.getHeaders());
+            logger.info(response.getStatusCode());
         } catch (IOException ex) {
             throw ex;
         }
